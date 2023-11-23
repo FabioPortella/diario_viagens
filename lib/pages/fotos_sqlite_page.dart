41e14f58619fb7d38dd6162cc06b5965f912f728
FabioPortella <fabioportella2009@gmail.com>
2023-11-23 14:39:42 -0300

    iniciando pag fotos e restaurando repository foto

---
+++ lib/pages/fotos_sqlite_page.dart
@@ -0,0 +1,273 @@
+import 'package:diario_viagens/model/viagem_sqlite_model.dart';
+import 'package:diario_viagens/pages/card_page.dart';
+import 'package:diario_viagens/repositories/viagem_sqlite_repository.dart';
+import 'package:diario_viagens/shared/widgets/text_label.dart';
+import 'package:flutter/material.dart';
+
+class FotosPageSQLite extends StatefulWidget {
+  const FotosPageSQLite({super.key});
+
+  @override
+  State<FotosPageSQLite> createState() => _FotosPageSQLiteState();
+}
+
+class _FotosPageSQLiteState extends State<FotosPageSQLite> {
+  ViagemSQLiteRepository viagemRepository = ViagemSQLiteRepository();
+  var _viagem = const <ViagemSQLiteModel>[];
+  var localViagemController = TextEditingController(text: "");
+  var dataInicioViagemController = TextEditingController(text: "");
+  var dataFinalViagemController = TextEditingController(text: "");
+  var apenasNaoEncerradas = false;
+  // ignore: prefer_typing_uninitialized_variables
+  var dataInicio;
+  // ignore: prefer_typing_uninitialized_variables
+  var dataFinal;
+
+  @override
+  void initState() {
+    super.initState();
+    obterViagem();
+  }
+
+  void obterViagem() async {
+    _viagem = await viagemRepository.obterDados(apenasNaoEncerradas);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Diário de Viagens"),
+        backgroundColor: Colors.green,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          localViagemController.text = "";
+          dataInicioViagemController.text = "";
+          dataInicio = null;
+          dataFinalViagemController.text = "";
+          dataFinal = null;
+          showDialog(
+              context: context,
+              builder: (BuildContext bc) {
+                return AlertDialog(
+                  alignment: Alignment.centerLeft,
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  title: const Text(
+                    "Adicionar",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  content: StatefulBuilder(builder: (context, setState) {
+                    return Wrap(
+                      children: [
+                        const TextLabel(texto: "Viagem para:"),
+                        TextField(
+                          controller: localViagemController,
+                        ),
+                        const TextLabel(texto: "Data o início da viagem"),
+                        TextField(
+                            controller: dataInicioViagemController,
+                            readOnly: true,
+                            onTap: () async {
+                              dataInicio = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(2000, 1, 1),
+                                  lastDate: DateTime(2050, 12, 31));
+                              if (dataInicio != null) {
+                                dataInicioViagemController.text =
+                                    "${dataInicio.day}/${dataInicio.month}/${dataInicio.year}";
+                              }
+                            }),
+                        const TextLabel(texto: "Data final"),
+                        TextField(
+                            controller: dataFinalViagemController,
+                            readOnly: true,
+                            onTap: () async {
+                              dataFinal = await showDatePicker(
+                                  context: context,
+                                  initialDate: DateTime.now(),
+                                  firstDate: DateTime(2000, 1, 1),
+                                  lastDate: DateTime(2050, 12, 31));
+                              if (dataFinal != null) {
+                                dataFinalViagemController.text =
+                                    "${dataFinal.day}/${dataFinal.month}/${dataFinal.year}";
+                              }
+                            }),
+                      ],
+                    );
+                  }),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Cancelar")),
+                    TextButton(
+                        onPressed: () async {
+                          if (localViagemController.text.trim().length < 3) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text(
+                                        "O local da viagem dever ser preenchido - 3 caracteres ou mais.")));
+                            return;
+                          }
+                          if (dataInicioViagemController.text == "") {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text(
+                                        "Data de início da viagem inválida")));
+                            return;
+                          }
+                          if (dataFinalViagemController.text == "") {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text(
+                                        "Data final da viagem inválida. Favor informar uma data, mesmo que seja uma previsão.")));
+                            return;
+                          }
+                          await viagemRepository.salvar(ViagemSQLiteModel(
+                              0,
+                              localViagemController.text,
+                              dataInicioViagemController.text,
+                              dataFinalViagemController.text,
+                              false));
+                          // ignore: use_build_context_synchronously
+                          Navigator.pop(context);
+                          obterViagem();
+                          setState(() {});
+                          // ignore: use_build_context_synchronously
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text("Viagem salva com sucesso")));
+                        },
+                        child: const Text("Salvar")),
+                  ],
+                );
+              });
+        },
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Viagens não encerradas",
+                    style: TextStyle(fontSize: 20, color: Colors.grey[800]),
+                  ),
+                  Switch(
+                      value: apenasNaoEncerradas,
+                      onChanged: (bool value) {
+                        apenasNaoEncerradas = value;
+                        obterViagem();
+                      })
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _viagem.length,
+                itemBuilder: (BuildContext bc, int index) {
+                  var viagem = _viagem[index];
+                  return Dismissible(
+                    onDismissed: (DismissDirection dismissDirection) async {
+                      await viagemRepository.remover(viagem.id);
+                      obterViagem();
+                      // ignore: use_build_context_synchronously
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Viagem foi excluida")));
+                    },
+                    confirmDismiss: (direction) async {
+                      return await showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text("Confirmar"),
+                            content: const Text(
+                                "Você realmente quer excluir este item?"),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(false),
+                                child: const Text("Cancelar"),
+                              ),
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.of(context).pop(true),
+                                child: const Text("Excluir"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    key: Key(viagem.localViagem),
+                    child: GestureDetector(
+                      // também pode ser usado onLongPress
+                      onTap: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const CardBasicRoute()));
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                          side: const BorderSide(
+                            color: MyColorsSample.primary,
+                            width: 2,
+                          ),
+                        ),
+                        elevation: 0,
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        child: Container(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Row(
+                                children: [
+                                  Text(
+                                    viagem.localViagem,
+                                    style: TextStyle(
+                                        fontSize: 24, color: Colors.grey[800]),
+                                  ),
+                                  const Spacer(),
+                                  Switch(
+                                    onChanged: (bool value) async {
+                                      viagem.encerrada = value;
+                                      viagemRepository.atualizar(viagem);
+                                      obterViagem();
+                                    },
+                                    value: viagem.encerrada,
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                  "Início: ${viagem.dataInicio} \nFim: ${viagem.dataFinal}",
+                                  style: TextStyle(
+                                      fontSize: 15, color: Colors.grey[700])),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
